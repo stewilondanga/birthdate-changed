68b10bc5ca33a2d88457f9c13e980ea36b938be8
Stewart ilondanga <stilondanga@gmail.com>
2019-02-14 22:59:14 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -53,30 +53,30 @@ function isDate(txtDate) {
   var currVal = txtDate;
   if (currVal == '')
     return true;
+
+  //Declare Regex
+  var rxDatePattern = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/;
+  var dtArray = currVal.match(rxDatePattern); // is format OK?
   /*
-    //Declare Regex
-    var rxDatePattern = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/;
-    var dtArray = currVal.match(rxDatePattern); // is format OK?
-
-    if (dtArray == null)
-      return false;
-
-    //Checks for dd/mm/yyyy format.
-    var dtDay = dtArray[1];
-    var dtMonth = dtArray[3];
-    var dtYear = dtArray[5];
-
-    if (dtMonth < 1 || dtMonth > 12)
-      return false;
-    else if (dtDay < 1 || dtDay > 31)
-      return false;
-    else if ((dtMonth == 4 || dtMonth == 6 || dtMonth == 9 || dtMonth == 11) && dtDay == 31)
-      return false;
-    else if (dtMonth == 2) {
-      var isleap = (dtYear % 4 == 0 && (dtYear % 100 != 0 || dtYear % 400 == 0));
-      if (dtDay > 29 || (dtDay == 29 && !isleap))
+      if (dtArray == null)
         return false;
-    }
 
-    return true;
-  }
+      //Checks for dd/mm/yyyy format.
+      var dtDay = dtArray[1];
+      var dtMonth = dtArray[3];
+      var dtYear = dtArray[5];
+
+      if (dtMonth < 1 || dtMonth > 12)
+        return false;
+      else if (dtDay < 1 || dtDay > 31)
+        return false;
+      else if ((dtMonth == 4 || dtMonth == 6 || dtMonth == 9 || dtMonth == 11) && dtDay == 31)
+        return false;
+      else if (dtMonth == 2) {
+        var isleap = (dtYear % 4 == 0 && (dtYear % 100 != 0 || dtYear % 400 == 0));
+        if (dtDay > 29 || (dtDay == 29 && !isleap))
+          return false;
+      }
+
+      return true;
+    }
